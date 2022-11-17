0eda8d4faf6f2f6970912f44b778fc5124fc8b40
Thales <thales.souz@outlook.com>
2022-11-17 19:26:05 -0300

    feat: req 02 finalizado

--- src/Services/Cars.service.ts
+++ src/Services/Cars.service.ts
@@ -13,6 +13,22 @@ class CarService {
     const car = this.carDomain(response);
     return car;
   }
+
+  public async get(): Promise<Car[]> {
+    const carODM = new CarODM();
+    const response = await carODM.get();
+    const carsResponse = response.map((car) => this.carDomain(car));
+    return carsResponse;
+  }
+
+  public async getById(id: string): Promise<Car | undefined> {
+    const carODM = new CarODM();
+    const response = await carODM.getById(id);
+    if (response) {
+      const car = this.carDomain(response);
+      return car;
+    }
+  }
 }
 
 export default CarService;
