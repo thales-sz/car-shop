e79e48bfd974c523d639013f5af875891bc83ad6
Thales <thales.souz@outlook.com>
2022-11-17 19:25:29 -0300

    feat: middleware de erro corrigido

--- src/Middlewares/ErrorHandler.ts
+++ src/Middlewares/ErrorHandler.ts
@@ -1,13 +1,21 @@
 import { NextFunction, Request, Response } from 'express';
 
 class ErrorHandler {
-  public static handle(
-    error: Error,
-    _req: Request,
-    res: Response,
-    next: NextFunction,
-  ) {
-    res.status(500).json({ message: error.message });
+  public static handle(error: Error, _req: Request, res: Response, next: NextFunction) {
+    switch (error.name) {
+      case 'CastError':
+        res.status(422).json({ message: 'Invalid mongo id' });
+        next();
+        break;
+      case 'NotFoundError':
+        res.status(404).json({ message: 'Not found' });
+        next();
+        break;
+      default:
+        res.status(500).json({ message: error.message });
+        next();
+        break;
+    }
     next();
   }
 }
