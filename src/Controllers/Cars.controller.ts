5ee0553a8b6f0684a2d5bd23ebf4b0f50eee6d3d
Thales <thales.souz@outlook.com>
2022-11-17 17:48:57 -0300

    feat: rota e controller criadas

--- src/Controllers/Cars.controller.ts
+++ src/Controllers/Cars.controller.ts
@@ -4,17 +4,24 @@ import CarService from '../Services/Cars.service';
 
 export default class CarController {
   private service: CarService;
-  constructor() {
+  private req: Request;
+  private res: Response;
+  private next: NextFunction;
+
+  constructor(req: Request, res: Response, next: NextFunction) {
+    this.req = req;
+    this.res = res;
+    this.next = next;
     this.service = new CarService();
   }
 
-  public async create(req: Request, res: Response, next: NextFunction) {
-    const newCar: ICar = req.body;
+  public async create() {
+    const newCar: ICar = this.req.body;
     try {
       const result = await this.service.create(newCar);
-      return res.status(201).json({ result });
+      return this.res.status(201).json({ result });
     } catch (error) {
-      next(error);
+      this.next(error);
     }
   }
 }
--- src/Routes/Cars.routes.ts
+++ src/Routes/Cars.routes.ts
@@ -1,7 +1,11 @@
 import * as express from 'express';
+import CarController from '../Controllers/Cars.controller';
 
 const router = express.Router();
 
-router.get('/');
+router.post(
+  '/', 
+  (req, res, next) => new CarController(req, res, next).create(),
+);
 
 export default router;
