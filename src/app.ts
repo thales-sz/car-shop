83e0da9fbe7d3b9a3b1f0cdb7c2769e25df158a8
Thales <thales.souz@outlook.com>
2022-11-17 17:38:36 -0300

    feat: middleware de error

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
 import express from 'express';
+import ErrorHandler from './Middlewares/ErrorHandler';
 import carsRoute from './Routes';
 
 const app = express();
 
 app.use(express.json());
+
 app.use('/cars', carsRoute);
+
+app.use(ErrorHandler.handle);
 
 export default app;
