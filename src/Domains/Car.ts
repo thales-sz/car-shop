6dc0dd3f66ed7f974b3ca85a7337ad4253add0e2
Thales <thales.souz@outlook.com>
2022-11-17 15:43:08 -0300

    feat: domain de Car corrigido

--- src/Domains/Car.ts
+++ src/Domains/Car.ts
@@ -1,22 +1,22 @@
 import ICar from '../Interfaces/ICar';
 
-export default class Car implements ICar {
-  public id?: string | undefined;
-  public doorsQty: number;
-  public seatsQty: number;
-  public model: string;
-  public year: number;
-  public buyValue: number;
-  public color: string;
-  public status?: boolean | undefined;
+export default class Car {
+  protected id?: string | undefined;
+  private doorsQty: number;
+  private seatsQty: number;
+  protected model: string;
+  protected year: number;
+  protected buyValue: number;
+  protected color: string;
+  protected status?: boolean | undefined;
 
-  constructor(objIcar: ICar) {
-    this.seatsQty = objIcar.seatsQty;
-    this.doorsQty = objIcar.doorsQty;
-    this.status = objIcar.status;
-    this.year = objIcar.year;
-    this.color = objIcar.color;
-    this.buyValue = objIcar.buyValue;
-    this.model = objIcar.model;
+  constructor(car: ICar) {
+    this.seatsQty = car.seatsQty;
+    this.doorsQty = car.doorsQty;
+    this.status = car.status;
+    this.year = car.year;
+    this.color = car.color;
+    this.buyValue = car.buyValue;
+    this.model = car.model;
   }
 }
