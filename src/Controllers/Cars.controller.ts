4e87efa9baaaf4084551f4b2d272c8c89940541f
Thales <thales.souz@outlook.com>
2022-11-17 17:37:46 -0300

    feat: controller de Cars

--- src/Controllers/Cars.controller.ts
+++ src/Controllers/Cars.controller.ts
@@ -1,13 +1,20 @@
 import { NextFunction, Request, Response } from 'express';
 import ICar from '../Interfaces/ICar';
+import CarService from '../Services/Cars.service';
 
 export default class CarController {
-  private carService: CarService;
+  private service: CarService;
   constructor() {
-    this.carService = new CarService();
+    this.service = new CarService();
   }
 
-  public create(req: Request, res: Response, next: NextFunction) {
+  public async create(req: Request, res: Response, next: NextFunction) {
     const newCar: ICar = req.body;
+    try {
+      const result = await this.service.create(newCar);
+      return res.status(201).json({ result });
+    } catch (error) {
+      next(error);
+    }
   }
 }
