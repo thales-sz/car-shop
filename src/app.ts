9339b3374d300c9ebd35ed6b9b1cafc93c3f0173
Thales <thales.souz@outlook.com>
2022-11-18 14:31:49 -0300

    feat: finalizado a rota /motorcycles

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,13 @@
 import express from 'express';
 import ErrorHandler from './Middlewares/ErrorHandler';
-import carsRoute from './Routes';
+import { carsRoute, motorcycleRoute } from './Routes';
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/cars', carsRoute);
+app.use('/motorcycles', motorcycleRoute);
 
 app.use(ErrorHandler.handle);
 
