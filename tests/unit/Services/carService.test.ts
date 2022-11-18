8110063edacfc9f2d7ff923d27649a087b9b8057
Thales <thales.souz@outlook.com>
2022-11-18 09:31:44 -0300

    test: teste da função "create" e "get"

--- tests/unit/Services/carService.test.ts
+++ tests/unit/Services/carService.test.ts
@@ -1,6 +1,9 @@
 import { expect } from 'chai';
 import { describe } from 'mocha';
+import { Model } from 'mongoose';
+import Sinon from 'sinon';
 import Car from '../../../src/Domains/Car';
+import CarService from '../../../src/Services/Cars.service';
 
 const returnMock = {
   id: '6377777fbdc1fc289e0a4070',
@@ -25,5 +28,23 @@ const requestMock = {
 const instance = new Car(returnMock);
 
 describe('Camada Service de Car', () => {
+  afterEach(function () {
+    Sinon.restore();
+  });
   
+  it('Ao chamar a função "create" com sucesso é registrado um novo carro', async function () {
+    Sinon.stub(Model, 'create').resolves(returnMock);
+    const service = new CarService();
+    const result = await service.create(requestMock);
+
+    expect(result).to.be.eqls(instance);
+  });
+
+  it('Deve retornar um array de Car ao chamar a função "get"', async function () {
+    Sinon.stub(Model, 'find').resolves([returnMock, returnMock, returnMock]);
+    const service = new CarService();
+    const result = await service.get();
+
+    expect(result).to.be.eqls([instance, instance, instance]);
+  });
 });
